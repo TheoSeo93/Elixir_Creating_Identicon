da58464da9e12adfa7c8f6050e57797ae4f165f6
Theo <youngho.seo@stonybrook.edu>
2018-08-17 00:22:03 -0400

    done

---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule  Identicon.Image do
+  # List of numbers default value of nil
+  # %Identicon.Image{}
+  # Identicon.Image{hex: nil}
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
+end
